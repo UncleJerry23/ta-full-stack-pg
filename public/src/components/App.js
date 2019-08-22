019e47876e919ead48f678ebce5953ea1bc46cb2
UncleJerry <jared.myhrberg@gmail.com>
2019-08-22 10:42:05 -0700

    basic splash

--- public/src/components/App.js
+++ public/src/components/App.js
@@ -4,16 +4,17 @@ import {
   Switch,
   Route
 } from 'react-router-dom';
-import NavBar from '../components/NavBar';
-import './appStyle.css';
+// import './appStyle.css';
+import NavBar from '../components/nav/NavBar';
+import Splash from './splash/Splash';
 
 export default function App() {
   return (
     <Router >
     <NavBar />
     <Switch>
-      {/* <Route exact path="/" component={Splash} />
-      <Route path="/search/:searchTerm" component={ErrContainer} />
+      <Route exact path="/" component={Splash} />
+      {/* <Route path="/search/:searchTerm" component={ErrContainer} />
       <Route path="/newErr" component={NewErr} />
       <Route path="/displayErr/:errId" component={DisplayError} /> */}
     </Switch>
---
+++ public/src/components/splash/Splash.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './splashStyle.css';
+
+function Splash() {
+  return (
+    <section className={styles.splash}>
+      <h1>O hey, check out all these cool bugs in here!!!</h1>
+    </section>
+  )
+}
+
+export default Splash;
